4f3f3a69239d367446e6bab7e063d1d5e3e8112e
riki89 <saikoudiallo59@gmail.com>
2021-07-02 22:20:10 -0500

    filtering

--- Assignments/Jobs/public/angular-app/skills/skills.controller.js
+++ Assignments/Jobs/public/angular-app/skills/skills.controller.js
@@ -1,13 +1,43 @@
 angular.module("jobApp").controller("SkillsController", SkillsController);
 
-function SkillsController($routeParams){
+function SkillsController(JobDataFactory, $routeParams, $window){
     const vm = this;
-    JobDataFactory.getAllSkills().then(function(response){
+    const jobId = $routeParams.jobId;
+    vm.jobId = jobId;
+    JobDataFactory.getAllSkills(jobId).then(function(response){
+        console.log("rep", response.skills);
+        vm.skills = response.skills;
+    });
+    JobDataFactory.getOneSkill(jobId).then(function(response){
         vm.job = response;
     });
     const skillId = $routeParams.skillId;
-    JobDataFactory.getOneSkill(skillId).then(function(response){
-        vm.job = response;
-    });
+    vm.delete = function(jobId, skillId){
+        JobDataFactory.deleteSkill(jobId, skillId).then(function(response){
+            $window.location.reload();
+            console.log("Skill deleted", response);
+           
+        }).catch(function(error){
+            console.log("Error deleting", error);
+        });
+
+    }
+
+    vm.addSkill = function(){
+        console.log("Add Skill......");
+        const postData = {
+            name : vm.newSkill
+        };
+        if (vm.skillForm.$valid){
+            JobDataFactory.addSkill(jobId, postData)
+                .then(function(response){
+                    $window.location.reload();
+                    console.log(response);
+                }).catch(function(error){
+                    console.log(error);
+                })
+        }
+        
+    }
 
 }
