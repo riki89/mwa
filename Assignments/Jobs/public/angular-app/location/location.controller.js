193dd7fabe4c4da1bb3afb39211599bda3ad2e3f
riki89 <saikoudiallo59@gmail.com>
2021-07-02 23:22:03 -0500

    last commit

--- Assignments/Jobs/public/angular-app/location/location.controller.js
+++ Assignments/Jobs/public/angular-app/location/location.controller.js
@@ -1,6 +1,6 @@
 angular.module("jobApp").controller("LocationController", LocationController);
 
-function LocationController(JobDataFactory, $routeParams){
+function LocationController(JobDataFactory, $routeParams, $window){
     const vm = this;
     const jobId = $routeParams.jobId;
     JobDataFactory.getLocation(jobId).then(function(response){
@@ -16,5 +16,22 @@ function LocationController(JobDataFactory, $routeParams){
             console.log("Error deleting", error);
         });
 
+    }
+    vm.addLocation = function(){
+        console.log("Add Location......");
+        const postData = {
+            address : vm.newAddress,
+            state: vm.newState
+        };
+        if (vm.locationForm.$valid){
+            JobDataFactory.addLocation(jobId, postData)
+                .then(function(response){
+                    $window.location.reload();
+                    console.log(response);
+                }).catch(function(error){
+                    console.log(error);
+                })
+        }
+        
     }
 }
